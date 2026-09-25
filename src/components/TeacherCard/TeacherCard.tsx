591ecd2ef060fb902fdd6b7db82be8bbe7a71b23
arstrong <arsendacenko89@gmail.com>
2026-09-25 10:26:02 +0100

    levels added

--- src/components/TeacherCard/TeacherCard.tsx
+++ src/components/TeacherCard/TeacherCard.tsx
@@ -18,67 +18,75 @@ const TeacherCard = ({teacher}: TeacherCardProps) => {
             </div>
 
         </div>
-        <div className={style.infoWrapper} >
-            <div className={style.teacherInfo}>
-                <span className={style.infoHeader}>
-                    <div className={style.languagesTitle}>
-                        <p>Languages</p>
-                        <h3>{teacher.name} {teacher.surname}</h3>
-                    </div>
-                </span>
-                <span className={style.teacherInfoDesc}>
-                    <div className={style.infoDescItems}>
-                        <span className={style.descItem}>
-                            <svg width={16} height={16}>
-                                <use className={style.bookIcon} href={`${sprite}#icon-book`}></use>
-                            </svg>
+        <div className={style.infoWrapper}>
+            <div className={style.teacherInfoWrapper}>
+                <div className={style.teacherInfo}>
+                    <span className={style.infoHeader}>
+                        <div className={style.languagesTitle}>
+                            <p>Languages</p>
+                            <h3>{teacher.name} {teacher.surname}</h3>
+                        </div>
+                    </span>
+                    <span className={style.teacherInfoDesc}>
+                        <div className={style.infoDescItems}>
+                            <span className={style.descItem}>
+                                <svg width={16} height={16}>
+                                    <use className={style.bookIcon} href={`${sprite}#icon-book`}></use>
+                                </svg>
 
-                            <p>Lessons online</p>
-                        </span>
+                                <p>Lessons online</p>
+                            </span>
 
-                        <span className={style.descItem}>
-                            <p>Lessons done: {teacher.lessons_done}</p>
-                        </span>
+                            <span className={style.descItem}>
+                                <p>Lessons done: {teacher.lessons_done}</p>
+                            </span>
 
-                        <span className={style.descItem}>
-                            <svg width={16} height={16}>
-                                <use className={style.starIcon} href={`${sprite}#icon-star`}></use>
-                            </svg>
+                            <span className={style.descItem}>
+                                <svg width={16} height={16}>
+                                    <use className={style.starIcon} href={`${sprite}#icon-star`}></use>
+                                </svg>
 
-                            <p>Rating: {teacher.rating}</p>
-                        </span>
+                                <p>Rating: {teacher.rating}</p>
+                            </span>
 
-                        <span className={style.descItem}>
-                            <p>Price / 1 hour: <span className={style.lightGreen}>{teacher.price_per_hour}$</span></p>
-                        </span>
-                    </div>
+                            <span className={style.descItem}>
+                                <p>Price / 1 hour: <span className={style.lightGreen}>{teacher.price_per_hour}$</span></p>
+                            </span>
+                        </div>
 
+                        <svg width={26} height={26}>
+                            <use className={style.heartIcon} href={`${sprite}#icon-heart`}></use>
+                        </svg>
+                    </span>
+                </div>
 
-                    <svg width={26} height={26}>
-                        <use className={style.heartIcon} href={`${sprite}#icon-heart`}></use>
-                    </svg>
-                </span>
+                <div className={style.teacherTalents}>
+                    <p className={style.talent}>
+                        <span className={style.talentLabel}>Speaks: </span>
+                        <span className={style.languages}>{teacher.languages.join(', ')}</span>
+                    </p>
+
+                    <p className={style.talent}>
+                        <span className={style.talentLabel}>Lesson Info: </span>
+                        {teacher.lesson_info}
+                    </p>
+
+                    <p className={style.talent}>
+                        <span className={style.talentLabel}>Conditions: </span>
+                        {teacher.conditions.join(' ')}
+                    </p>
+                </div>
+
+                <span className={style.readMeSpan}>Read more</span>
             </div>
 
-            <div className={style.teacherTalents}>
-                <p className={style.talent}>
-                    <span className={style.talentLabel}>Speaks: </span>
-                    <span className={style.languages}>{teacher.languages.join(', ')}</span>
-                </p>
-
-                <p className={style.talent}>
-                    <span className={style.talentLabel}>Lesson Info: </span>
-                    {teacher.lesson_info}
-                </p>
-
-                <p className={style.talent}>
-                    <span className={style.talentLabel}>Conditions: </span>
-                    {teacher.conditions.join(' ')}
-                </p>
-            </div>
-            <div className={style.levels}>
-
-            </div>
+            <ul className={style.levels}>
+                {teacher.levels.map((level) => (
+                    <li key={level} className={style.level}>
+                        #{level}
+                    </li>
+                ))}
+            </ul>
         </div>
     </div>
   )
